8a7ef211d31b90107252408c67f74beb55e12506
Yanting14 <yanting.or.2023@scis.smu.edu.sg>
2024-11-07 17:30:36 +0800

    changed file structures

---
+++ mentors/mentorSignUp.js
@@ -0,0 +1,173 @@
+// Import Firestore and Auth from Firebase
+import { getFirestore, doc, setDoc, getDoc } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
+import { getAuth, createUserWithEmailAndPassword, fetchSignInMethodsForEmail } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Get form and input elements
+    const form = document.querySelector('form');
+    const fullNameInput = document.querySelector('input[aria-label="Full Name"]');
+    const usernameInput = document.querySelector('input[aria-label="Username"]');
+    const emailInput = document.querySelector('input[aria-label="Email"]');
+    const phoneInput = document.querySelector('input[aria-label="Phone Number"]');
+    const passwordInput = document.querySelector('input[aria-label="Password"]');
+    const confirmPasswordInput = document.querySelector('input[aria-label="Confirm password"]');
+    const termsCheckbox = document.querySelector('input#termsAndConditions');
+
+    // Initialize Firestore and Auth
+    const db = getFirestore();
+    const auth = getAuth();
+
+    // Form submission event listener
+    form.addEventListener('submit', async function (event) {
+        event.preventDefault();
+        clearErrors();
+
+        const loading = true
+        const button  = document.getElementById('submitButton')
+        const spinner = document.getElementById('spinner')
+
+        button.classList.add('hidden')
+        spinner.classList.remove('hidden')
+
+        // Perform form validation
+        const isValid = await validateForm();
+
+        const fullName = fullNameInput.value;
+        const username = usernameInput.value;
+        const email = emailInput.value;
+        const phone = phoneInput.value;
+        const password = passwordInput.value;
+        
+        if (isValid) {
+            try {
+                // Attempt to create the user in Firebase Authentication
+                const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+                const user = userCredential.user;
+
+                // Store user data in Firestore
+                await setDoc(doc(db, "Mentors", username), {
+                    uid: user.uid,
+                    name: fullName,
+                    username: username,
+                    email: email,
+                    phone: phone,
+                });
+
+                // Store username in session
+                sessionStorage.setItem('username', username);
+                sessionStorage.setItem('name', fullName)
+                // next page
+                window.location.href = "mentorCredentials.html";
+            } catch (error) {
+                if(error.code === 'auth/email-already-in-use'){
+                    showError(emailInput, 'This email has already been registered')
+                }
+                console.error("Error adding document: ", error);
+
+            }
+        }
+        else{
+            button.classList.remove('hidden')
+            spinner.classList.add('hidden')
+        }
+    });
+
+    async function validateForm() {
+        let valid = true;
+
+        // Full Name Validation
+        if (fullNameInput.value.trim() === '') {
+            showError(fullNameInput, 'Full Name is required');
+            valid = false;
+        } else if (containsNumbers(fullNameInput.value)) {
+            showError(fullNameInput, 'Full name cannot contain numbers');
+            valid = false;
+        }
+
+        // Username Validation
+        if (usernameInput.value.trim() === '') {
+            showError(usernameInput, 'Username is required');
+            valid = false;
+        } else {
+            const usernameExists = await checkUsernameExists(usernameInput.value);
+            if (usernameExists) {
+                showError(usernameInput, 'Username is already taken');
+                valid = false;
+            }
+        }
+
+        // Email Validation
+        const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailPattern.test(emailInput.value)) {
+            showError(emailInput, 'Please enter a valid email');
+            valid = false;
+        } else {
+            const emailExists = await checkEmailExists(emailInput.value);
+            if (emailExists) {
+                showError(emailInput, 'This email is already registered');
+                valid = false;
+            }
+        }
+
+        // Phone Number Validation
+        const phonePattern = /^[0-9]+$/;
+        if (!phonePattern.test(phoneInput.value) || phoneInput.value.trim() === '' || phoneInput.value.length < 8) {
+            showError(phoneInput, 'Please enter a valid phone number (e.g., 93210001)');
+            valid = false;
+        }
+
+        // Password and Confirm Password Validation
+        if (passwordInput.value.trim() === '') {
+            showError(passwordInput, 'Password is required');
+            valid = false;
+        }
+        if (confirmPasswordInput.value.trim() === '') {
+            showError(confirmPasswordInput, 'Confirm Password is required');
+            valid = false;
+        } else if (passwordInput.value !== confirmPasswordInput.value) {
+            showError(confirmPasswordInput, 'Passwords do not match');
+            valid = false;
+        }
+
+        // Terms and Conditions Checkbox Validation
+        if (!termsCheckbox.checked) {
+            showError(termsCheckbox, 'You must agree to the terms and conditions');
+            valid = false;
+        }
+
+        return valid;
+    }
+
+    function showError(inputElement, message) {
+        const errorElement = document.createElement('small');
+        errorElement.classList.add('text-red-600');
+        errorElement.textContent = message;
+
+        if (inputElement.type === 'checkbox') {
+            document.getElementById("tNcError").appendChild(errorElement);
+        } else {
+            inputElement.parentElement.appendChild(errorElement);
+        }
+        inputElement.classList.add('is-invalid');
+    }
+
+    function clearErrors() {
+        document.querySelectorAll('.text-red-600').forEach(error => error.remove());
+        document.querySelectorAll('.is-invalid').forEach(input => input.classList.remove('is-invalid'));
+    }
+
+    function containsNumbers(str) {
+        return str.split('').some(char => !isNaN(char) && char !== ' ');
+    }
+
+    async function checkUsernameExists(username) {
+        const docRef = doc(db, "Mentors", username);
+        const docSnap = await getDoc(docRef);
+        return docSnap.exists();
+    }
+
+    async function checkEmailExists(email) {
+        const methods = await fetchSignInMethodsForEmail(auth, email);
+        return methods.length > 0;
+    }
+});
---
+++ profile/profile.js
@@ -0,0 +1,192 @@
+import { db, auth, app } from '../firebaseconfig.js';
+import { collection, doc, setDoc, getDoc } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
+
+const vueApp = Vue.createApp({
+    data() {
+        return {
+            profile: {
+                picture:"https://via.placeholder.com/150",
+                firstName: '',
+                lastName: '',
+                email: '',
+                phone: '',
+                address: '',
+                summary: '',
+                shortinfo:'',
+                education: [
+                    {
+                        school:"",
+                        degree:"",
+                        startDate:"",
+                        endDate:""
+
+                    }
+                ],
+                socialLinks: {
+                    linkedin: '',
+                    github: ''
+                },
+                about: 'Recent computer science graduate passionate about web development and AI. Looking for opportunities to grow in the tech industry.',
+                industries: ['Education and Training', 'Information Technology', 'Finance and Banking', 'Healthcare','Social Science','Law','Media & Communications','Marketing & Advertising','Retail and Customer Service','Hospitality and Tourism',"Engineering"],
+                selectedIndustries: [],
+                resume: false,
+                assessmentCompleted: false
+            },
+            skills: [],
+            percentage : 0,
+            newSkill :"",
+            maxSelections: 3,
+            uid: "DYLUE1cZeESWzlQnGfVAAW4iZUI2"
+
+        }
+    },
+    methods: {
+        calculateProgressBar(){
+            this.$nextTick(() => {
+                try {
+                    const total = document.getElementsByClassName("criterial").length;
+                    const success = document.getElementsByClassName("text-success").length;
+                    this.percentage = (success / total) * 100;
+                } catch (error) {
+                    console.error("Error calculating progress: ", error);
+                }
+            });
+        },
+        addEducation(){
+            this.profile.education.push({
+                school:"",
+                degree:"",
+                startDate:"",
+                endDate:""
+            })
+        },
+        removeEducation(index) {
+            this.profile.education.splice(index, 1)
+        },
+        addSkill() {
+            if (this.newSkill.trim() && !this.skills.includes(this.newSkill.trim())) {
+              this.skills.push(this.newSkill.trim())
+              this.newSkill = ''
+            }
+        },
+        removeSkill(index) {
+            this.skills.splice(index, 1)
+        },
+        async saveProfile() {
+            try {
+                const user = auth.currentUser;
+                if (!user) {
+                    alert("Please log in to save your profile.");
+                    return;
+                }
+        
+                // Save to Firestore using user email as the document ID
+                const userDocRef = doc(db, "users", user.email);
+        
+                // Check if the document already exists
+                const userDoc = await getDoc(userDocRef);
+        
+                if (userDoc.exists()) {
+                    // Document exists, so update it (merge fields)
+                    await setDoc(userDocRef, this.profile, { merge: true });
+                    console.log("Profile updated successfully!");
+                } else {
+                    // Document does not exist, create a new one
+                    await setDoc(userDocRef, this.profile);
+                    console.log("New profile created successfully!");
+                }
+        
+                alert('Profile Saved!');
+        
+        
+                // Hide the modal
+                const modalElement = document.getElementById('editProfileModal');
+                const modalInstance = bootstrap.Modal.getInstance(modalElement);
+                if (modalInstance) {
+                    modalInstance.hide();
+                }
+            } catch (error) {
+                console.error("Error writing profile:", error);
+            }
+        },
+        triggerFileInput() {
+            this.$refs.fileInput.click();
+        },
+        onFileChange(event) {
+            const file = event.target.files[0];
+            if (file) {
+                const reader = new FileReader();
+                reader.onload = (e) => {
+                    this.profile.picture = e.target.result;
+                };
+                reader.readAsDataURL(file);
+            }
+        },
+        async fetchProfile() {
+            try {
+                const user = auth.currentUser;
+                if (user) {
+                    const docRef = doc(db, "users", user.email);
+                    const docSnap = await getDoc(docRef);
+    
+                    if (docSnap.exists()) {
+                        const fetchedData = docSnap.data();
+                        this.profile = { ...this.profile, ...fetchedData };
+                        this.profile.email = fetchedData.email;
+                        console.log("Profile data:", this.profile);
+                    } else {
+                        console.log("No such document!");
+                    }
+                } else {
+                    console.log("User is not logged in.");
+                }
+            } catch (error) {
+                console.error("Error fetching profile:", error);
+            }
+        },
+        isMaxSelected(industry) {
+            return this.profile.selectedIndustries.length >= this.maxSelections &&
+                   !this.profile.selectedIndustries.includes(industry);
+        },
+        isBasicInfoComplete() {
+            return this.profile.firstName && this.profile.lastName && this.profile.email && this.profile.phone && this.profile.address;
+        },
+        isSkillsAdded() {
+            return this.skills.length > 0;
+        },
+        isCareerInterestsComplete() {
+            return this.profile.selectedIndustries.length > 0;
+        },
+        isResumeUploaded() {
+            return this.profile.resume;
+        },
+        isAssessmentCompleted() {
+            return this.profile.assessmentCompleted;
+        }
+    },
+    mounted(){
+        this.calculateProgressBar();
+        this.fetchProfile();
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+                this.fetchProfile(user.email);
+            } else {
+                console.log("User is not logged in.");
+            }
+        });
+    },
+    watch: {
+        profile: {
+            handler() {
+                this.calculateProgressBar();
+            },
+            deep: true
+        },
+        skills() {
+            this.calculateProgressBar();
+        }
+    }
+
+}).mount("#app");
+
