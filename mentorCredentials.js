5acb53da603ce9024903dada126c29d27f517578
kesterfun <165992263+kesterfun@users.noreply.github.com>
2024-11-07 17:44:41 +0800

    firebase config changes

--- mentorCredentials.js
+++ mentorCredentials.js
@@ -1,17 +1,6 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
-import { getFirestore, doc, getDoc ,updateDoc } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
+import {doc, getDoc ,updateDoc } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
 
-// Initialize Firebase
-const firebaseConfig = {
-    apiKey           : "AIzaSyCaCMLfzaAjedNd7ITsMmFwoskhIboREf0",
-    authDomain       : "wadii-career-20ae3.firebaseapp.com",
-    projectId        : "wadii-career-20ae3",
-    storageBucket    : "wadii-career-20ae3.appspot.com",
-    messagingSenderId: "967171133152",
-    appId            : "1:967171133152:web:42cac325a66903712bca85"
-};
-const app = initializeApp(firebaseConfig);
-const db  = getFirestore(app);
+import {db} from './firebaseconfig.js'
 
 const API_KEY = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiYTU0NjM2NmQtZDI0OC00NTE5LWI5MjMtNTVkZjQxOWQwZGI4IiwidHlwZSI6ImFwaV90b2tlbiJ9.FO6LLfcb6v-L_HtOE9mPyYSGpTlNunYkmZhVCQkyLHs";
 
