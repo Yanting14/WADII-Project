70beddc1e858482a9b0c8a6650215655832a3f3b
Yanting14 <yanting.or.2023@scis.smu.edu.sg>
2024-11-10 23:58:27 +0800

    key

--- firebaseconfig.js
+++ firebaseconfig.js
@@ -1,12 +1,20 @@
+// firebaseConfig.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
-import * as config from './firebaseconfig.js';  
 
+// Firebase configuration
 
-console.log(config);
-const app = initializeApp(config);
+const firebaseConfig = {
+   apiKey: "AIzaSyD2yjbcfhGB3MdgSoWVn-OoBEn6ErUO634",
+   authDomain: "wadii-career-20ae3.firebaseapp.com",
+   projectId: "wadii-career-20ae3",
+   storageBucket: "wadii-career-20ae3.appspot.com",
+   messagingSenderId: "967171133152",
+   appId: "1:967171133152:web:42cac325a66903712bca85"
+};
+const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
 
-export { db, auth, app };
+export { db, auth, app};
