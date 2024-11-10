6a95838f9a2ef56670d7cf0b2d80a3f0e6a6c0a1
Yanting14 <yanting.or.2023@scis.smu.edu.sg>
2024-11-10 23:49:08 +0800

    change stuff

--- firebaseconfig.js
+++ firebaseconfig.js
@@ -1,7 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
-import config from './firebaseconfig.js';  
+import * as config from './firebaseconfig.js';  
 
 const app = initializeApp(config);
 const db = getFirestore(app);
