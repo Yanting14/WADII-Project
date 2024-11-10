72e5ebddb3be3e44686de9d46bf1e81c58b6c8ed
Yanting14 <yanting.or.2023@scis.smu.edu.sg>
2024-11-10 23:40:01 +0800

    edit api key

---
+++ firebaseconfig.js
@@ -0,0 +1,10 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
+import config from './firebaseconfig.js';  
+
+const app = initializeApp(config);
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+export { db, auth, app };
